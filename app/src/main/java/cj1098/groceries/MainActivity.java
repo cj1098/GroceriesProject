06ec641da4f59e44850ceddcbff62702b2b2ceeb
chris <cj1098silver@gmail.com>
2014-12-12 04:18:17 -0600

    follow up commit trying to delete a package folder.

--- app/src/main/java/cj1098/groceries/MainActivity.java
+++ app/src/main/java/cj1098/groceries/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setLogo(R.drawable.abc_btn_radio_material);
     }
 
 
